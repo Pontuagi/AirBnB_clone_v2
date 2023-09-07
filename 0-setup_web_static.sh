3821896b183434210c6a91c9b7a22342d94ef000
Pontuagi <benepontuagi@gmail.com>
2023-09-07 23:24:58 +0000

    commit errors

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,34 +1,94 @@
 #!/usr/bin/env bash
-#Bash script that sets up your web servers for the deployment of web_static
+# a Bash script that sets up my web servers for the deployment of web_static.
 
-#Nginx Installation
-sudo apt-get update
-sudo apt-get -y install nginx
-sudo ufw allow 'Nginx HTTP'
+# Install Nginx if not already installed
+if ! dpkg -l | grep -q "nginx"; then
+        sudo apt-get update
+        sudo apt install -y nginx
+        sudo systemctl start nginx
+        sudo systemctl enable nginx
+        echo "Nginx Installed"
+fi
 
-#Creates folders and files if it doesn't exist
-sudo mkdir -p /data/
-sudo mkdir -p /data/web_static/
-sudo mkdir -p /data/web_static/releases/
-sudo mkdir -p /data/web_static/shared/
-sudo mkdir -p /data/web_static/releases/test/
-sudo touch /data/web_static/releases/test/index.html
-sudo echo "<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>" | sudo /data/web_static/releases/test/index.html
+data_folder="/data/"
+# Create /data/ if it !exists
+if [ ! -d "$data_folder" ]; then
+        sudo mkdir -p "$data_folder"
+        echo "/data/ directory created"
+fi
 
-#Creates symbolic link
-sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
+web_static_folder="/data/web_static/"
+# Create the folder /data/web_static/ if it !exist
+if [ ! -d "$web_static_folder" ]; then
+        sudo mkdir -p "$web_static_folder"
+        echo "/data/web_static/ directoy created"
+fi
 
-#Gives ownership of the /data/ folder to the ubuntu user AND group
-sudo chown -R ubuntu:ubuntu /data/
+web_static_rel_dir="/data/web_static/releases/"
+# Create the folder /data/web_static/releases/ if it !exist
+if [ ! -d "$web_static_rel_dir" ]; then
+        sudo mkdir -p "$web_static_rel_dir"
+        echo "/data/web_static/releases/ directory created"
+fi
 
-#Updates Nginx configuration
-sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
+web_static_shared="/data/web_static/shared/"
+# Create the folder /data/web_static/shared/ if it !exist
+if [ ! -d "$web_static_shared" ]; then
+        sudo mkdir -p "$web_static_shared"
+        echo "/data/web_static/shared/ directory created"
+fi
 
-#Restarts Nginx after configuration
-sudo service nginx restart
+# Create the folder /data/web_static/releases/test/ if it doesn’t already exist
+if [ ! -d "/data/web_static/releases/test/" ]; then
+        sudo mkdir -p "/data/web_static/releases/test/"
+        echo "/data/web_static/releases/test/ directory created"
+fi
+
+# create a fake HTML file with simple content
+html_file="/data/web_static/releases/test/index.html"
+sudo mkdir -p '$(dirname "$html_file")'
+echo "<html>
+        <head>
+                <title> **Test Page** </title>
+        </head>
+        <body>
+                <h1> Hello, It mr Pontuagi!</h1>
+        </body>
+     </html>" | sudo tee "$html_file" > /dev/null
+
+echo "test HTML file created"
+
+# create a symbolic link /data/web_static/current
+# linked to the /data/web_static/releases/test/ folder
+
+symbolic_link="/data/web_static/current"
+target_dir="/data/web_static/releases/test/"
+
+# remove existing symbolic link if it exists
+if [ -L "$symbolic_link" ]; then
+        sudo rm "$symbolic_link"
+fi
+
+
+# Create a new symbolic_link
+sudo ln -s "$target_dir" "$symbolic_link"
+
+echo "symbolic link created"
+
+# Give ownership of the /data/ folder to the ubuntu user AND group
+user="ubuntu"
+group="ubuntu"
+sudo chown -R "$user:$group" "/data/"
+
+nginx_config="/etc/nginx/sites-available/default"
+alias_config="location /hbnb_static/ {
+        alias /data/web_static/current/;
+        index index.html;
+        }"
+sudo sed -i "s|location / {|location / { $alias_config|g" "$nginx_config"
+if sudo nginx -t; then
+    sudo systemctl restart nginx
+    echo "Nginx configuration updated and Nginx restarted."
+else
+    echo "Nginx configuration test failed. Please check your configuration."
+fi
--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,20 +1,24 @@
 #!/usr/bin/python3
 """
-Fabric script that generates a .tgz archive from the contents
-of the web_static folder of your AirBnB Clone repo, using the
-function do_pack
+a Fabric script that generates a .tgz archive from the contents of the
+web_static folder of AirBnB clone
 """
 
 from fabric.api import local
-from time import strftime
-from datetime import date
+from datetime import datetime
+
 
 def do_pack():
-    """ A script generating archive the contents of web_static folder"""
-    dates = strftime("%Y%m%d%H%M%S")
+    """ 
+    Generates a .tgz archive from the contents of the web_static folder
+    """
+    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+    archive_name = "web_static_" + timestamp + ".tgz"
+    archive_path = "versions/" + archive_name
+
     try:
         local("mkdir -p versions")
-        local("tar -czvf versions/web_static_{}.tgz web_static/".format(dates))
-        return "versions/web_static_{}.tgz".format(dates)
-    except Exception as e:
+        local("tar -cvzf {} web_static".format(archive_path))
+        return archive_path
+    except Exception:
         return None
