dfec52e36cc7aa14f5dd46b43629d8b28f3f62da
Pontuagi <benepontuagi@gmail.com>
2023-09-07 15:15:26 +0300

    compression script

---
+++ 1-pack_web_static.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+a Fabric script that generates a .tgz archive from the contents of the
+web_static folder of AirBnB clone
+"""
+
+from fabric.api import local
+from datetime import datetime
+
+
+def do_pack():
+    """
+    Generates a .tgz archive from the contents of the web_static folder
+    """
+    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+    archive_name = "web_static_" + timestamp + ".tgz"
+    archive_path = "versions/" + archive_name
+
+    try:
+        local("mkdir -p versions")
+        local("tar -cvzf {} web_static".format(archive_path))
+        return archive_path
+    except Exception:
+        return None
