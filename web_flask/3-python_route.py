580287bf096594f9a753c070c45b61bfdaa7f5e8
Pontuagi <benepontuagi@gmail.com>
2023-09-23 14:47:30 +0300

    python route

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+
+"""
+A script that starts a Flask web application:
+Web application is listening on 0.0.0.0, port 5000
+"""
+
+from flask import Flask, escape
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hbnb_home():
+    """ display for / """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ Content for route /hbnb"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    """ Render route /c/text content """
+    # Replacing underscores with spaces
+    text = escape(text).replace('_', ' ')
+    return "C {}".format(text)
+
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_is_fun(text):
+    """ Render route /python/text content """
+    # Replacing underscores with spaces
+    text = escape(text).replace('_', ' ')
+    return "Python {}".format(text)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
