2ff653165a459eb3cd85bb04e29b2c90ac53e501
Pontuagi <benepontuagi@gmail.com>
2023-09-23 14:30:32 +0300

    c route

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+A script that starts a Flask web application
+The application is listening on 0.0.0.0, port 5000
+"""
+
+from flask import Flask, escape
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hbnb_home():
+    """ display for / """
+    return ("Hello HBNB!")
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ Content for route /hbnb"""
+    return ("HBNB")
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    """Render route /c/text content"""
+    # Replacing underscores with spaces
+    text = escape(text).replace('_', ' ')
+    return ("C {}".format(text))
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
