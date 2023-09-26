21699ec501a950a1343fdc63437cae394decef24
Pontuagi <benepontuagi@gmail.com>
2023-09-26 11:23:12 +0300

    Cities by state

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -5,17 +5,16 @@ The application is listening on 0.0.0.0, port 5000
 """
 
 from flask import Flask, render_template
-from models import storage, State
+from models import storage, State, City
 
 app = Flask(__name__)
 
 
-@app.route('/states_list', strict_slashes=False)
-def states_list():
-    """ Display a list of states """
-    states = storage.all(State).values()
-    sorted_states = sorted(states, key=lambda state: state.name)
-    return render_template('7-states_list.html', states=sorted_states)
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """ Display a list of states and cities """
+    states = sorted(storage.all(State).values(), key=lambda state: state.name)
+    return render_template('8-cities_by_states.html', states=states)
 
 
 @app.teardown_appcontext
